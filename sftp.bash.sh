bec4f014ef8630b860cc5963875bcc6b02c4f6c6
leev667@yahoo.com <root@web1clone.symphony.local>
2023-06-29 12:10:46 +0000

    First Upload

---
+++ sftp.bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp -p /etc/ssh/sshd_config{,.$(date +%Y%m%d-%H%M)}
+sshd -t
+sdiff -s /etc/ssh/sshd_config{,.20210511-1159}
+groupadd sftponly
+newgrp sftponly
+useradd darthsftp -s /sbin/nologin -G sftponly
+getent passwd darthsftp
+systemctl restart sshd
+su -s /bin/bash -c 'touch /home/darthsftp/testfile.txt' darthsftp
+cd /home/darthsftp/
+mkdir .ssh
+cd .ssh/
+ssh-keygen -f darthsftp
+tail -1 darthsftp.pub >> authorized_keys
+chown darthsftp. -R /home/darthsftp/.ssh
+chown root:root /home/darthsftp
+chmod 755 /home/darthsftp
+sftp -P -i /home/darthsftp/.ssh/darthsftp darthsftp@127.0.0.1
